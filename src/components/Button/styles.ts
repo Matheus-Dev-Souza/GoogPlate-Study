beeee3d007cb120f0a6a2b2da0f78c0b81430a85
Mateus <goncalvesouza1996@gmail.com>
2023-08-05 15:19:09 -0300

    first commit

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+
+export const styles = StyleSheet.create({
+    container: {
+        width: '56',
+        height: '56',
+        borderRadius: 7,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#2E9D4C',
+        position:"absolute",
+        rightMargin:42,
+        top: 42,
+        zIndex: 100
+    }
+});
